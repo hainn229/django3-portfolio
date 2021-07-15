be596d5bdd820516c1669a368ba5bd31b8123081
Nguyen Ngoc Hai <69133089+hainn229@users.noreply.github.com>
2021-07-15 23:21:20 +0700

    update

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.all_blogs, name='all_blogs'),
-    path('<int:blog_id>', views.details, name='details'),
+    path('', views.blogs, name='blogs'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 from .models import Project
-# Create your views here.
-
 
 def home(request):
-    # projects = Project.objects.all()
-    projects = Project.objects.order_by('-id')
-    return render(request, 'portfolio/home.html', {'projects': projects})
+    projects = Project.objects.all()
+    return render(request, 'portfolio/home.html', {'projects':projects})
